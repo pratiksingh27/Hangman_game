748debf19f1cd587b6f458914a42c44882c778b5
pratiksingh27 <pratik27march@gmail.com>
2023-09-21 01:48:56 +0530

    finishing project

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,23 @@
 const keyboardDiv = document.querySelector(".keyboard");
 const wordDispaly = document.querySelector(".word-display");
 const guessedText = document.querySelector(".guessed-text b");
-const hangemanImg= document.querySelector(".hangman-box img")
+const hangemanImg= document.querySelector(".hangman-box img");
+const gameModel = document.querySelector(".game-model");
+const palyAgainBtn = document.querySelector(".play-again");
 
-let currentWord, wrongGuessCount =0;
+let currentWord, correctLetters, wrongGuessCount;
 const maxCount =6;
+
+const resetGame = () => {
+    //resetting game
+    correctLetters =[];
+    wrongGuessCount =0;
+    hangemanImg.src = `images/hangman-${wrongGuessCount}.svg`;
+    guessedText.innerHTML = `${wrongGuessCount} / ${maxCount}`;
+    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    wordDispaly.innerHTML =currentWord.split("").map(() => `<li class="letter"></li>`).join("");
+    gameModel.classList.remove("show");
+}
 
 const getRandomWord = () =>{
     //selecting a randomword and hint from word list
@@ -12,7 +25,19 @@ const getRandomWord = () =>{
     console.log(word);
     currentWord =word;
     document.querySelector(".hint-text b").innerText = hint;
-    wordDispaly.innerHTML =word.split("").map(() => `<li class="letter"></li>`).join("");
+    resetGame();
+   /*  wordDispaly.innerHTML =word.split("").map(() => `<li class="letter"></li>`).join(""); */
+}
+
+const gameOver = (isVictory) =>{
+    //after 600ms of game complete.. showing model with relevent detials
+   setTimeout(() => {
+    const modelText = isVictory ? `You Found the word: ` : 'the correct word was: ';
+    gameModel.querySelector("img").src=`images/${isVictory ? 'victory' : 'lost'}.gif`;
+    gameModel.querySelector("h4").innerText=`${isVictory ? 'congrats' : 'Game Over'}`;
+    gameModel.querySelector("p").innerHTML=`${modelText} <b>${currentWord}</b>`;
+    gameModel.classList.add("show");
+   }, 300);
 }
 
 const initGame = (button, clickedLetter) =>{
@@ -23,6 +48,7 @@ const initGame = (button, clickedLetter) =>{
         // console.log(clickedLetter, "is exist on word");
         [...currentWord].forEach((letter, index)=>{
             if(letter === clickedLetter){
+                correctLetters.push(letter);
                 wordDispaly.querySelectorAll("li")[index].innerHTML = letter;
                 wordDispaly.querySelectorAll("li")[index].classList.add("guessed");
             }
@@ -34,10 +60,14 @@ const initGame = (button, clickedLetter) =>{
     }
     button.disabled =true;
     guessedText.innerHTML = `${wrongGuessCount} / ${maxCount}`;
+
+    //calling gameover function if any of this condition meets
+    if(wrongGuessCount === maxCount) return gameOver(false);
+    if(correctLetters.length === currentWord.length) return gameOver(true);
 }
 
 //creating keyboard button
-for(let i=97;i<122; i++){
+for(let i=97;i<123; i++){
     const button = document.createElement("button");
     button.innerHTML = String.fromCharCode(i);
     keyboardDiv.appendChild(button);
@@ -46,3 +76,4 @@ for(let i=97;i<122; i++){
 }
 
 getRandomWord();
+palyAgainBtn.addEventListener("click", getRandomWord);
